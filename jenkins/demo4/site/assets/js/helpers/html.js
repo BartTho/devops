beb873a7381ebaad802ee1d7d22c4eac62a29727
BartTho <61376948+BartTho@users.noreply.github.com>
2024-08-29 10:15:35 +0200

    Delete jenkins/demo4/site directory

--- jenkins/demo4/site/assets/js/helpers/html.js
+++
@@ -1,3 +0,0 @@
-export default (strings, ...values) => (
-  strings.reduce((acc, str, index) => `${acc}${str}${(values[index] || '')}`, '')
-);
--- jenkins/demo4/site/assets/js/mixins/styles.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-
-Vue.mixin({
-  created() {
-    if (this.$options.styles) {
-      this.styles = this.$options.styles;
-    }
-  },
-});
